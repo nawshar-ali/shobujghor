b5d0524b690818f786dc9cb0edc0cdf12aa7b5eb
nawshar-ali <nowhser.ali28@gmail.com>
2024-04-07 13:21:39 +0600

    add category related classes

---
+++ utility/src/main/java/com/shobujghor/app/utility/dto/CategoryDto.java
@@ -0,0 +1,19 @@
+package com.shobujghor.app.utility.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto implements Serializable {
+    private String name;
+    private String shortDescription;
+    private String longDescription;
+    private String logoUrl;
+}
