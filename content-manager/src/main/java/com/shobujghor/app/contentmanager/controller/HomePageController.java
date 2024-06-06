606ec37add6d70c53a87510d1a5481b368e1f2b3
nawshar-ali <nowhser.ali28@gmail.com>
2024-06-06 16:28:48 +0600

    cache category data in content-manager

--- content-manager/src/main/java/com/shobujghor/app/contentmanager/controller/HomePageController.java
+++ content-manager/src/main/java/com/shobujghor/app/contentmanager/controller/HomePageController.java
@@ -4,7 +4,6 @@ import com.shobujghor.app.contentmanager.service.HomePageService;
 import com.shobujghor.app.utility.response.contentmanager.HomePageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- content-manager/src/main/java/com/shobujghor/app/contentmanager/service/HomePageServiceImpl.java
+++ content-manager/src/main/java/com/shobujghor/app/contentmanager/service/HomePageServiceImpl.java
@@ -1,21 +1,43 @@
 package com.shobujghor.app.contentmanager.service;
 
 import com.shobujghor.app.contentmanager.api.InventoryClient;
+import com.shobujghor.app.contentmanager.repository.redis.CategoryRedisRepository;
+import com.shobujghor.app.utility.constants.RedisCacheKeys;
+import com.shobujghor.app.utility.dto.CategoryDto;
 import com.shobujghor.app.utility.response.contentmanager.HomePageResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class HomePageServiceImpl implements HomePageService {
 
     private final InventoryClient inventoryClient;
+    private final CategoryRedisRepository categoryRedisRepository;
+    private final long CATEGORY_LIST_CACHE_TIMEOUT = 1800;
 
     @Override
     public HomePageResponse getHomePageData() {
-        var categoryListResponse = inventoryClient.getCategories();
         return HomePageResponse.builder()
-                .categories(categoryListResponse.getCategories())
+                .categories(getCategories())
                 .build();
+    }
+
+    private List<CategoryDto> getCategories() {
+        var categoryListResponseOpt = categoryRedisRepository.getData(RedisCacheKeys.CATEGORY_LIST.name());
+
+        if (categoryListResponseOpt.isPresent()) {
+            log.info("Found categories in cache");
+            return categoryListResponseOpt.get().getCategories();
+        }
+
+        var categoryListResponse = inventoryClient.getCategories();
+        categoryRedisRepository.saveDataWithExpiry(RedisCacheKeys.CATEGORY_LIST.name(), categoryListResponse, CATEGORY_LIST_CACHE_TIMEOUT, TimeUnit.SECONDS);
+        return categoryListResponse.getCategories();
     }
 }
